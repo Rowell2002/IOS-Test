1f861c833614f014ea172a679d0bd9c316950c5f
Rowell2002 <chethanarowell2002@gmail.com>
2026-07-02 22:39:26 +0530

    Tap frenzy and Light It Up (Basic)

---
+++ IOS-Dev/IOS-Dev/IOS_DevApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_DevApp.swift
+//  IOS-Dev
+//
+//  Created by Chethana Rowell on 2026-06-07.
+//
+
+import SwiftUI
+
+@main
+struct IOS_DevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
